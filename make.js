7acfba796893ae49ba874898aac664aa6a71ab2f
shubhamsharma8700 <91122681+shubhamsharma8700@users.noreply.github.com>
2022-01-20 10:38:32 +0530

    Add files via upload

---
+++ make.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
